7041668fcb67230c68b3d1f2d6e843cec5f9d582
Imran <imranhsayed@gmail.com>
2021-10-31 19:56:17 +0530

    Update files

--- 01-check-leap-year/example-two.py
+++ 01-check-leap-year/example-two.py
@@ -1,12 +1,12 @@
-'''
+"""
 Required Condition to be met: For a year to be a leap year, it needs to be divisible by 4
 and if it's a century year then it also needs to be divisible by 400.
-'''
+"""
 
 # Take the input from the user and store the number into a variable called year.
 # Also convert the number into integer so we can perform conditional numeric operations.
 year = int(input('Enter the year: '))
-isLeapYear = False
+is_leap_year = False
 
 # Condition one - If number is divisible by 4 and has remainder as zero.
 if year % 4 == 0:
@@ -15,17 +15,17 @@ if year % 4 == 0:
 	if year % 100 == 0:
 		# When it's  a century year, we also need to check if it's divisible by 400.
 		if year % 400 == 0:
-			isLeapYear = True
+			is_leap_year = True
 		else:
-			isLeapYear = False
+			is_leap_year = False
 	else:
-		isLeapYear = True  # If not century year and divisible by 4.
+		is_leap_year = True  # If not century year and divisible by 4.
 
 else:
-	isLeapYear = False
+	is_leap_year = False
 
-# If isLeapYear is True, then print the message.
-if isLeapYear:
+# If is_leap_year is True, then print the message.
+if is_leap_year:
 	print('{0} is a leap year'.format(year))
 else:
 	print('{0} is not leap year'.format(year))
