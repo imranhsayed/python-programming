3f499746fd82df8143ff0a2bce34d5eb18db38d9
Imran <imranhsayed@gmail.com>
2021-11-06 20:03:22 +0530

    add update record

---
+++ 05-database-example/6-update-records.py
@@ -0,0 +1,26 @@
+import mysql.connector
+
+try:
+	database_connection = mysql.connector.connect(
+		host="localhost",
+		port=8889,
+		user="root",
+		password="root",
+		database="SELECTION_DB"
+	)
+	connection_cursor = database_connection.cursor()
+
+	sql_statement = """UPDATE EMP_SELECTION SET AGE=AGE+1 WHERE GENDER='MALE'"""
+
+	connection_cursor.execute(sql_statement)
+
+	database_connection.commit()
+	print(connection_cursor.rowcount, "record(s) updated.")
+
+except mysql.connector.Error as error:
+	print("parameterized query failed {}".format(error))
+finally:
+	if database_connection.is_connected():
+		connection_cursor.close()
+		database_connection.close()
+		print("MySQL connection is closed")
